714968258ff1fc0e57c555a083bd83f0fcb5b097
Bourouail Issam <issambourouail33@gmail.com>
2025-10-11 04:38:55 +0200

    feat: add Country business/data classes and support Add/Update modes in Person form

--- DVLD_(Driving License Management System)/DVLD_(Driving License Management System)/People/frmAddUpdatePerson.cs
+++ DVLD_(Driving License Management System)/DVLD_(Driving License Management System)/People/frmAddUpdatePerson.cs
@@ -12,9 +12,19 @@ namespace DVLD__Driving_License_Management_System_.People
 {
     public partial class frmAddUpdatePerson : Form
     {
+
+
+        public enum enMode { AddNew = 0, Update = 1 };
+        private int _PersonID = -1;
+
         public frmAddUpdatePerson()
         {
             InitializeComponent();
+        }
+        public frmAddUpdatePerson(int PersonID )
+        {
+            InitializeComponent();
+            this._PersonID = PersonID;
         }
     }
 }
