5aa9fb9fc3d6e5290ebe797b7eea5acdc7b3054c
Bourouail Issam <issambourouail33@gmail.com>
2025-11-20 06:04:34 +0100

    feat: add License entity with issue reason types
    Create License class with properties, constructors, and issue reason enum.
    Add corresponding data access layer class template.

---
+++ DVLD_(Driving License Management System)/DVLD_BuisnessDVLD_Buisness/clsLicense.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BuisnessDVLD_Buisness
+{
+    public  class clsLicense
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public clsLicenseClass LicenseClassIfo;
+
+        public enum enIssueReason { FirstTime = 1, Renew = 2, DamagedReplacement = 3, LostReplacement = 4 };
+
+        public int LicenseID { set; get; }
+        public int ApplicationID { set; get; }
+        public int DriverID { set; get; }
+        public int LicenseClass { set; get; }
+        public DateTime IssueDate { set; get; }
+        public DateTime ExpirationDate { set; get; }
+        public string Notes { set; get; }
+        public float PaidFees { set; get; }
+        public bool IsActive { set; get; }
+        public enIssueReason IssueReason { set; get; }
+        public int CreatedByUserID { set; get; }
+
+        public string IssueReasonText
+        {
+            get
+            {
+                return GetIssueReasonText(this.IssueReason);
+            }
+        }
+
+
+        // ###################   Constructors   ###################
+
+        clsLicense()
+        {
+            this.LicenseID = -1;
+            this.ApplicationID = -1;
+            this.DriverID = -1;
+            this.LicenseClass = -1;
+            this.IssueDate = DateTime.Now;
+            this.ExpirationDate = DateTime.Now;
+            this.Notes = "";
+            this.PaidFees = 0;
+            this.IsActive = true;
+            this.IssueReason = enIssueReason.FirstTime;
+            this.CreatedByUserID = -1;
+
+            Mode = enMode.AddNew;
+        }
+
+        public clsLicense(
+            int LicenseID, int ApplicationID, int DriverID,
+            int LicenseClass,DateTime IssueDate, DateTime
+            ExpirationDate, string Notes,
+            float PaidFees, bool IsActive, 
+            enIssueReason IssueReason, int CreatedByUserID
+            )
+
+        {
+            this.LicenseID = LicenseID;
+            this.ApplicationID = ApplicationID;
+            this.DriverID = DriverID;
+            this.LicenseClass = LicenseClass;
+            this.IssueDate = IssueDate;
+            this.ExpirationDate = ExpirationDate;
+            this.Notes = Notes;
+            this.PaidFees = PaidFees;
+            this.IsActive = IsActive;
+            this.IssueReason = IssueReason;
+            this.CreatedByUserID = CreatedByUserID;
+
+            this.LicenseClassIfo = clsLicenseClass.Find(this.LicenseClass);
+
+            Mode = enMode.Update;
+        }
+
+        // ###################   Helps Methods   ###################
+
+        public static string GetIssueReasonText(enIssueReason IssueReason)
+        {
+
+            switch (IssueReason)
+            {
+                case enIssueReason.FirstTime:
+                    return "First Time";
+                case enIssueReason.Renew:
+                    return "Renew";
+                case enIssueReason.DamagedReplacement:
+                    return "Replacement for Damaged";
+                case enIssueReason.LostReplacement:
+                    return "Replacement for Lost";
+                default:
+                    return "First Time";
+            }
+        }
+    }
+}
