c3062145ab600576a22dab548dbdcfb6ec55185a
Bourouail Issam <issambourouail33@gmail.com>
2026-03-06 09:13:28 +0100

    Add connection string support via App.config and System.Configuration
    - Added DVLDConnection in App.config for database access.
    - Updated DVLD and DVLD_DataAccess projects to reference System.Configuration.
    - Modified clsDataAccessSettings to read connection string from App.config instead of hardcoding.

--- DVLD_(Driving License Management System)/DVLD_DataAccess/clsDataAccessSettings.cs
+++ DVLD_(Driving License Management System)/DVLD_DataAccess/clsDataAccessSettings.cs
@@ -1,13 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+
 
 namespace DVLD_DataAccess
 {
     public class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;DataBase=DVLD;User Id=sa;Password=sa123456;";
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager
+                       .ConnectionStrings["DVLDConnection"]
+                       .ConnectionString;
+            }
+        }
     }
 }
